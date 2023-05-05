ca73cb551344efdeda6e14c9b76d1fff67d27f3e
Alanis Ribeiro <126534588+alanisribeiro@users.noreply.github.com>
2023-05-05 19:07:57 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [value, setValue] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="box">
+      <h2>CONTADOR</h2>
+      <p>{value}</p>
+      <button className="btn" onClick={() => setValue(value + 1)}>Aumentar</button>
+      <button className="btn" onClick={() => setValue(value - 1)}>Diminuir</button>
+      <button className="btn" onClick={() => setValue(0)}>Resetar</button>
+
     </div>
-  );
+    </>
+  )
+    
 }
 
 export default App;
